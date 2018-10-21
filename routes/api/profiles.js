76306b82f1fc5ef2d8f85988b58c45fd69d41b6e
Mateusz <wszola.m@gmail.com>
2018-10-21 21:27:40 +0200

    Handle creating profile and validating the data

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 const mongoose = require('mongoose');
+
+// Load validation functions
+const validateProfileInput = require('../../validation/profile');
 
 // @route   GET api/profiles/
 // @desc    Get logged in user profile
@@ -13,7 +17,8 @@ router.get(
   (req, res, next) => {
     const errors = {};
 
-    Profile.findOne({ user: req.user.id })
+    Profile.findOne({ user: req.user._id })
+      .populate('user', ['name', 'username', 'date'])
       .then(profile => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user';
@@ -28,8 +33,90 @@ router.get(
   }
 );
 
-// @route   GET api/profiles/:profielId
-// @desc    Get profile by Id
+// @route   GET api/profiles/:profileID
+// @desc    Get profile by ID
 // @access  Public
+router.get('/:user_id', (req, res, next) => {
+  const { user_id } = req.params;
+  const errors = {};
+
+  Profile.findOne({ user: user_id })
+    .populate('user', ['name', 'username', 'date'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// @route   POST api/profiles/
+// @desc    Create or update user PROFILE
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    if (req.body['name'] && req.body['name'] !== undefined) {
+      User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $set: { name: req.body['name'] } },
+        { new: true }
+      )
+        .then(updatedUser =>
+          console.log('Name in user profile has been updated')
+        )
+        .catch(err => next(err));
+    }
+
+    const profileFields = {};
+    profileFields.user = req.user._id;
+    const standardFields = ['bio', 'location', 'website', 'birthday'];
+
+    standardFields.forEach(field => {
+      if (req.body[field]) {
+        profileFields[field] = req.body[field];
+      }
+    });
+
+    Profile.findOne({ user: req.user._id }).then(profile => {
+      if (profile) {
+        // Update the profile
+        Profile.findOneAndUpdate(
+          { user: req.user._id },
+          { $set: profileFields },
+          { new: true }
+        )
+          .then(updatedProfile => res.json(updatedProfile))
+          .catch(err => next(err));
+      } else {
+        // Create profile
+        const newProfile = new Profile(profileFields);
+        newProfile
+          .save()
+          .then(createdProfile => res.json(createdProfile))
+          .catch(err => next(err));
+      }
+    });
+
+    // For this route, validate and create or update only string information about profile (bio, location, website, ...)
+    // For things like followers, likes, tweets will be different routes
+    // !INCLUDE ability to change name
+
+    // 1. Validate req.body
+    // 2. Check if update profile or create new profile for the user
+  }
+);
 
 module.exports = router;
---
+++ validation/profile.js
@@ -0,0 +1,79 @@
+// Validation for creating or updating user PROFILE
+const validator = require('validator');
+const _ = require('lodash');
+
+module.exports = data => {
+  let errors = {};
+
+  // I don't know exactly what req.body will contain. Sometimes it will be full information about profile, and sometimes just some properties to update so I need to loop through every property in req.body object, make sure it is a string, and then validate it
+
+  const profileInformation = [
+    'bio',
+    'location',
+    'website',
+    'birthday',
+    'avatar',
+    'backgroundPicture',
+    'name' // This is an extra property which is placed in User model (but I would like to be able to change it with profile)
+  ];
+
+  const entries = Object.entries(data);
+  console.log('entries', entries);
+  entries.forEach(([property, value]) => {
+    data[property] = _.isEmpty(value) ? '' : value.toString();
+
+    // I do not specify isEmpty for things like bio, because user can fill empty bio to reset bio
+    // If req.body has a value, and the value is not empty, user do not want to clear the input, so I validate it
+
+    if (property === 'name') {
+      if (_.isEmpty(value)) {
+        errors.name = 'You need to specify a value for name field';
+      } else {
+        if (!validator.isAlphanumeric(value.split(' ').join(''), 'pl-PL')) {
+          errors.name = 'Invalid name format';
+        }
+        if (!validator.isLength(value, { min: 6, max: 25 })) {
+          errors.name = 'Name must be between 6 and 25 characters';
+        }
+      }
+    }
+
+    if (property === 'location') {
+      if (!_.isEmpty(value)) {
+        if (!validator.isAlphanumeric(value.split(' ').join(''), 'pl-PL')) {
+          errors.location = 'Invalid location';
+        }
+      }
+    }
+
+    if (property === 'website') {
+      if (!_.isEmpty(value)) {
+        if (!validator.isURL(value)) {
+          errors.website = 'Website must be a URL';
+        }
+      }
+    }
+
+    if (property === 'birthday') {
+      if (!_.isEmpty(value)) {
+        if (!_.isDate(value)) {
+          errors.birthday = 'Birthday must be a date';
+        }
+      }
+    }
+    // TODO:
+    // if (property === 'avatar') {
+    //   // Avatar will be uploaded by the user
+
+    // }
+
+    // if (property === 'backgroundPicture') {
+    //   // Background Picuture will be uploaded by the user
+    // }
+  });
+
+  return {
+    errors,
+    isValid: _.isEmpty(errors)
+  };
+};
