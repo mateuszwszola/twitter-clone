eecba80a474de98eacceecb4a408a9c3271e3c3b
Mateusz Wszola <wszola.m@gmail.com>
2019-10-06 22:47:51 +0200

    Try setup aws eb

--- validation/objectId.js
+++ validation/objectId.js
@@ -4,7 +4,6 @@ module.exports = id => {
   const idErrors = {};
 
   if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-    // id is not valid ObjectId, findOne with this value will cause an error
     idErrors.objectid = `ID ${id} is not a valid ObjectId`;
   }
 
