c4665c0d94be47e394b253d668febd49959b698e
Mateusz Wszola <wszola.m@gmail.com>
2019-10-08 19:36:32 +0200

    Add client build folder

---
+++ client/build/precache-manifest.4040493a750e55306b79d2c12f0f7601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc76c808f1262376b9da1b2025ed5d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "811a20f390bbfbc331a0",
+    "url": "/static/css/main.e2fbb550.chunk.css"
+  },
+  {
+    "revision": "279eb55530bdf25b5656",
+    "url": "/static/js/2.8b65f6ca.chunk.js"
+  },
+  {
+    "revision": "811a20f390bbfbc331a0",
+    "url": "/static/js/main.3098d7df.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  },
+  {
+    "revision": "9cd963f3b1e94d71f361d50b022e43aa",
+    "url": "/static/media/portret-placeholder.9cd963f3.png"
+  }
+]);
